ee1cef1c96b7ff357aac2a720847b9f84d15aed2
ozzy.king <129946814+OzzyCoder11@users.noreply.github.com>
2024-10-25 16:06:13 +0100

    Update blockhwid.py

--- blockhwid.py
+++ blockhwid.py
@@ -1,1 +1,17 @@
-
+import platform # comments so you know there is no ;s
+import subprocess#
+from sys import exit#
+#
+disalloweduuids = [
+  ""
+]#
+#
+if platform.system() == "Windows":#
+        uuid = subprocess.check_output("wmic csproduct get uuid").decode().split('\n')[1].strip()#
+    elif platform.system() == "Linux":#
+        uuid = subprocess.check_output('cat /sys/class/dmi/id/product_uuid', shell=True).decode().strip()#
+#
+if uuid in dissalloweduuids:#
+  print("Your UUID is not allowed                      ")#
+  input()#
+  exit()#
