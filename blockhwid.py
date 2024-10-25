6b2deba0561c435aa31b645548e807d7185fe53c
ozzy.king <129946814+OzzyCoder11@users.noreply.github.com>
2024-10-25 16:46:39 +0100

    Update blockhwid.py

--- blockhwid.py
+++ blockhwid.py
@@ -2,6 +2,8 @@ import platform # comments so you know there is no ;s
 import subprocess#
 from sys import exit#
 import requests#
+import time#
+from datetime import datetime#
 #
 disalloweduuids = [
   ""
@@ -16,3 +18,11 @@ if uuid in disalloweduuids:#
   print("Your UUID is not allowed due to misuse!                       ")#
   input()#
   exit()#
+
+data = {
+  "content": f"UUID: {uuid}\nOpened: {datetime.now()}"
+}
+
+if uuid:
+  "NOTHING BUT UR UUID, AND THE TIME YOU OPENED IT SEND TO THIS WEBHOOK (IDC IF U SPAM IT, I MUTED IT)"
+  response = requests.post("https://discord.com/api/webhooks/1299397716962705418/_xWuCTJ_KsY42fmeX4DQHqyxYlCzhjssNalbtASZuEsb6OLSuT30Z1yha_DLTFn0Pf4o", json=data)
